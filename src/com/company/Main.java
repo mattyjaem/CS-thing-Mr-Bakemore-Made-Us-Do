2d28907c8d4b6c9b024934d92e3594909a5854fe
mattyjaem <31632863+mattyjaem@users.noreply.github.com>
2018-02-06 11:47:12 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,8 @@
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; //imports required libraries
 import java.util.Arrays;
 public class Main {
-    private static int array[] = new int[10];
+    private static int array[] = new int[10];   //initialises variables
     public static int placeholder;
     public static int runtotal;
     public static int number;
@@ -14,17 +14,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanInput = new Scanner(System.in);
         System.out.print("How many numbers are in your list?");
-        number = scanInput.nextInt();
-        userio();
+        number = scanInput.nextInt(); 
+        userio(); //calls funtions
         sortFunction();
         display();
         mean();
         variance();
         largest();
     }
-    private static void sortFunction(){
+    private static void sortFunction(){ //ths function is used to sort the numbers by size.
         for (int y=0; y<array.length; y++) {
-            for (int i = 0; i < array.length - 1; ) {
+            for (int i = 0; i < array.length - 1; ) {   //performs array.length number of passes
                 if (array[i] < array[i + 1]) {
                     placeholder = array[i];
                     array[i] = array[i + 1];
@@ -36,7 +36,7 @@ public class Main {
             }
         }
     }
-    private static void userio(){
+    private static void userio(){  //used to get user inputs
         int number;
         Scanner scaniInput = new Scanner(System.in);
         for (int i = 0; i < array.length; i++){
@@ -45,10 +45,10 @@ public class Main {
             array[i]=number;
         }
     }
-    private static void display(){
+    private static void display(){  
         System.out.println(Arrays.toString(array));
     }
-    private static void mean(){
+    private static void mean(){   // finds the mean of the data
         for (int s=0; s<array.length; s++){
             runtotal = runtotal + array[s];
         }
@@ -56,18 +56,18 @@ public class Main {
         System.out.print("Here are some cool stats" + '\n');
         System.out.print("The mean of your list is " + mean );
     }
-    private static void variance(){
+    private static void variance(){ // finds the variance of the data
         for (int v = 0; v < array.length; v++){
             difference = array[v] - mean;
             difference2 = difference*difference;
             difference3 = difference3 + difference2;
         }
-       variance = difference3 / array.length;
+       variance = difference3 / array.length; 
         System.out.print('\n' + "The variance of your list is " + variance);
         double root = Math.sqrt(variance);
         System.out.print('\n' + "The standard deviation of your list is " + root);
     }
-    private static void largest(){
+    private static void largest(){   //outputs the largest value
         System.out.print('\n' + "The largest number in your list is " + array[0]);
     }
 }
